81a8abd48bebc642040814424c695c598fc74e69
Luciano Lapenna <lucianoglapenna@gmail.com>
2024-09-29 20:32:29 -0300

    fix: set correct type for people ids on reservation creation

--- app/reservation/route.ts
+++ app/reservation/route.ts
@@ -6,7 +6,7 @@ export async function POST(req: Request) {
 
   const date = new Date(body.date);
   const notes = body.notes as string;
-  const people = body.people as number[];
+  const people = body.people as string[];
 
   try {
     const user = await prisma.reservation.create({
@@ -14,23 +14,29 @@ export async function POST(req: Request) {
         date,
         notes,
         people: {
-          connect: people.map((id) => ({ id })),
+          connect: people.map((id) => ({ id: Number(id) })),
         },
       },
     });
 
     return NextResponse.json({ user }, { status: 201 });
   } catch (error) {
+    console.error(error);
     return NextResponse.json({ error }, { status: 500 });
   }
 }
 
 export async function GET() {
   try {
-    const reservations = await prisma.reservation.findMany();
+    const reservations = await prisma.reservation.findMany({
+      include: {
+        people: true,
+      },
+    });
 
     return NextResponse.json({ reservations });
   } catch (error) {
+    console.error(error);
     return NextResponse.json({ error }, { status: 500 });
   }
 }
