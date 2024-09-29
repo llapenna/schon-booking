8454b6d13d4a9ebf6bd6e9a46e8a28735180d32c
Luciano Lapenna <lucianoglapenna@gmail.com>
2024-09-29 20:37:47 -0300

    feat: fetch all data on server side

--- app/page.tsx
+++ app/page.tsx
@@ -1,36 +1,29 @@
 import { CalendarView } from "@/components/calendar";
+import { UserDialog } from "@/components/user-dialog";
+import { prisma } from "@/services/db";
+import { DataProvider } from "@/components/context/data";
 
 export const dynamic = "force-dynamic";
 
-export interface Reservation {
-  id: string;
-  people: string[];
-  date: string;
-}
+export default async function Home() {
+  const people = await prisma.person.findMany();
+  const reservations = await prisma.reservation.findMany({
+    include: {
+      people: true,
+    },
+  });
 
-const DATA: Reservation[] = [
-  {
-    id: "1",
-    people: ["Alice", "Bob"],
-    date: "2024-09-23",
-  },
-  {
-    id: "2",
-    people: ["Alice", "David"],
-    date: "2024-10-02",
-  },
-  {
-    id: "3",
-    people: ["Eve"],
-    date: "2024-10-21",
-  },
-];
-
-export default function Home() {
   return (
-    <main className=" p-6 w-screen h-screen flex flex-col gap-4 items-center justify-center">
-      <h1>Schön Booking</h1>
-      <CalendarView reservations={DATA}></CalendarView>
-    </main>
+    <DataProvider people={people} reservations={reservations}>
+      <main>
+        <nav className="flex flex-row justify-end p-2">
+          <UserDialog />
+        </nav>
+        <div className="p-6 w-screen h-screen flex flex-col gap-4 items-center justify-center">
+          <h1>Schön Booking</h1>
+          <CalendarView reservations={reservations}></CalendarView>
+        </div>
+      </main>
+    </DataProvider>
   );
 }
