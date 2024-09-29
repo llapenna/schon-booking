877b1d9a577439b6ebb64846284e5369416d0a08
Luciano Lapenna <lucianoglapenna@gmail.com>
2024-09-29 03:07:03 -0300

    fix: add padding to main body

--- app/page.tsx
+++ app/page.tsx
@@ -28,7 +28,7 @@ const DATA: Reservation[] = [
 
 export default function Home() {
   return (
-    <main className="w-screen h-screen flex flex-col gap-4 items-center justify-center">
+    <main className=" p-6 w-screen h-screen flex flex-col gap-4 items-center justify-center">
       <h1>Schön Booking</h1>
       <CalendarView reservations={DATA}></CalendarView>
     </main>
