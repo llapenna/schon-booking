1108f779dfe03d3abf5845fb6802791554800b59
Luciano Lapenna <lucianoglapenna@gmail.com>
2024-09-29 02:00:49 -0300

    feat: add colored dot to each day button

--- components/calendar/calendar-view/day.tsx
+++ components/calendar/calendar-view/day.tsx
@@ -1,10 +1,22 @@
 import { Button, DialogTrigger } from "@/components/ui";
 import { DayProps } from "./types";
 
-export const Day = ({ n, highlighted = false }: DayProps) => {
+export const Day = ({ n, onClick, withDot, highlighted = false }: DayProps) => {
+  const dotColor = highlighted ? "bg-red-600" : "bg-gray-400";
   return (
     <DialogTrigger asChild>
-      <Button variant={highlighted ? "outline" : "ghost"}>{n}</Button>
+      <Button
+        onClick={onClick}
+        variant={highlighted ? "outline" : "ghost"}
+        className="relative"
+      >
+        {n}
+        {withDot && (
+          <div
+            className={`absolute rounded-full ${dotColor} h-1.5 aspect-square top-1 right-1`}
+          ></div>
+        )}
+      </Button>
     </DialogTrigger>
   );
 };
